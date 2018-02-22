e3bd7157c50f9b16035e33a15715dffe53f81336
karolineMJ <karoline_Mj@hotmail.com>
2018-02-22 11:02:44 +0100

    exam  look good

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -1,17 +1,27 @@
-let bigCircle = document.querySelector("#BigCircle");
-let smallCircles = document.querySelectorAll(".smallJs1");
-
+let bigCircle = document.querySelectorAll(".circleContainer");
+//let smallCircles = document.querySelectorAll(".smallJs1");
 // onmouseover bigCircle add new class to smallCircles
 /*bigCircle.addEventListener("mouseover", circlesGetBig);
 function circlesGetBig (){
     console.log("yo");
 } */
-bigCircle.onmouseover = function () {
-    console.log("yo");
-    smallCircles.classList.add("moveCircles");
+bigCircle.forEach(big=>big.onmouseover = function (e) {
+    let elm = e.target;
+    console.log(elm);
+    let smallCircles = elm.querySelectorAll(".smallJs1");
+    smallCircles.forEach(bub=>bub.classList.add("moveCircles"));
     //circleGetBig()
-};
+});
+
+bigCircle.forEach(big=>big.onmouseout = function (e) {
+    let elm = e.target;
+    console.log(elm);
+    let smallCircles = elm.querySelectorAll(".smallJs1");
+    smallCircles.forEach(bub=>bub.classList.remove("moveCircles"));
+    //circleGetBig()
+});
 /*
+
 function circleGetBig() {
     console.log("yo");
 
